3a88b5fb94b62f6fb6e62b52eb0e7d81e5a816db
Mido191020 <at744245@gmail.com>
2024-05-20 00:59:37 +0300

    we finsh author list display in the page and handel the view

--- models/author.js
+++ models/author.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const { DateTime } = require("luxon");
+
 const AuthorSchema = new mongoose.Schema({
   firstNmae: {
     type: String,
@@ -23,4 +25,29 @@ AuthorSchema.virtual("name").get(function () {
 AuthorSchema.virtual("url").get(function () {
   return `/catalog/author/${this._id}`;
 });
+// Virtual for author's formatted lifespan
+AuthorSchema.virtual("lifespan").get(function () {
+  if (this.date_of_birth && this.date_of_death) {
+    // Format when both birth and death dates are present
+    return `${DateTime.fromJSDate(this.date_of_birth).toLocaleString(
+      DateTime.DATE_MED
+    )} - ${DateTime.fromJSDate(this.date_of_death).toLocaleString(
+      DateTime.DATE_MED
+    )}`;
+  } else if (this.date_of_birth) {
+    // Format when only birth date is present
+    return `${DateTime.fromJSDate(this.date_of_birth).toLocaleString(
+      DateTime.DATE_MED
+    )} `;
+  } else if (this.date_of_death) {
+    // Format when only death date is present
+    return ` ${DateTime.fromJSDate(this.date_of_death).toLocaleString(
+      DateTime.DATE_MED
+    )}`;
+  } else {
+    // Return an empty string if neither birth nor death dates are available
+    return "";
+  }
+});
+
 module.exports = mongoose.model("Author", AuthorSchema);
