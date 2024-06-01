e1bba4c9fc6420a862b118e52bed927d3df1fbc5
Mido191020 <at744245@gmail.com>
2024-06-01 14:23:53 +0300

    I've created a new database collection and fixed the issue with the corrupt data. Everything is functioning correctly now, except for the detailed page. When you click on any link, a 404 error occurs, indicating that the file cannot be found.

--- models/genre.js
+++ models/genre.js
@@ -2,14 +2,14 @@ const mongoose = require("mongoose");
 //const Schema = mongoose.Schema; // Import the Schema object
 
 const GenreSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        minLength: 3,
-        maxLength: 100,
-    },
+  name: {
+    type: String,
+    required: true,
+    minLength: 3,
+    maxLength: 100,
+  },
 });
-GenreSchema.virtual("url").get(function() {
-    return `/catalog/genre/${this._id}`;
+GenreSchema.virtual("url").get(function () {
+  return `/catalog/genre/${this._id}`;
 });
-module.exports = mongoose.model("genre", GenreSchema);
+module.exports = mongoose.model("Genre", GenreSchema);
